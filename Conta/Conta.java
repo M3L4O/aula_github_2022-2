62a3fc688139a54d1b32556bec009948643c8e30
vitin-m <vitormelo.lopes@hotmail.com>
2023-01-16 16:16:15 -0300

    define classe Conta

--- Conta/Conta.java
+++ Conta/Conta.java
@@ -1,5 +1,20 @@
 package Conta;
 
 public class Conta {
+    private double saldo;
+    private double limite;
     
+    public Conta(double limite) {
+        saldo = 0;
+        this.limite = limite;
+    }
+    
+    public void depositar(double qte) {
+        saldo += qte;
+    }
+    
+    public boolean sacar(double qte) {
+        if(saldo - qte < limite) return false;
+        else return true;
+    }
 }
