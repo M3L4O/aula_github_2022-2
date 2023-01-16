480dc83a71513f25082b5235ecf59e91353fcb75
Wesley Vitor <wesleyvitor37417@gmail.com>
2023-01-16 17:18:41 -0300

    implementada função de remover clientes #2

--- Main.java
+++ Main.java
@@ -1,35 +1,48 @@
 import java.util.Arrays;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import Cliente.Cliente;
-import Conta.Conta;
 
 
 public class Main {
-	
+
+	private static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+
 	public static void main(String[] args) {
-		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Adicionar Conta", "Sair"));
-	while(true) {
+		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Adicionar Cliente", "Remover Cliente", "Sair"));
 		
-		switch (mainMenu.getSelection()) {
-			case 1:
-				Scanner s = new Scanner(System.in);
-				System.out.println("Digite o seu nome:");
-				String name = s.nextLine();
-				System.out.println("Digite o seu cpf:");
-				String cpf = s.nextLine();
-				Cliente cliente = new Cliente(name, cpf);
-				clientes.add(cliente);
-				break;
-			case 2:
-				System.out.println("Fim");
-				return;
-			default:
-				break;
+		Scanner s = new Scanner(System.in);
+		String cpf;
+
+		while(true) {
+
+			switch (mainMenu.getSelection()) {
+				case 1:
+					System.out.println("Digite o seu nome:");
+					String name = s.nextLine();
+					System.out.println("Digite o seu cpf:");
+					cpf = s.nextLine();
+					Cliente cliente = new Cliente(name, cpf);
+					clientes.add(cliente);
+					System.out.println(name);
+					break;
+				case 2:
+					System.out.println("Digite o cpf do cliente para remover:");
+					cpf = s.nextLine();
+
+					removerCliente(cpf);
+					break;
+				case 3:
+					System.out.println("Fim");
+					return;
+				default:
+					break;
+			}
+
 		}
-		
 	}
+
+	private static void removerCliente(String cpf){
+		clientes.removeIf(item -> item.getCpf().compareTo(cpf) == 0);
 	}
 }
